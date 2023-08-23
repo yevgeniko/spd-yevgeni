9d5be0ec1994aacc274b72a1b353d985ac301103
h74.amirLitan <amirlitan@gmail.com>
2023-08-23 15:41:47 +0300

    add class client manager

---
+++ inc/client_manager.hpp
@@ -0,0 +1,41 @@
+#ifndef CLIENT_SERVER_MANAGER_HPP
+#define CLIENT_MANAGER_HPP
+
+#include <QObject>
+#include <QList>
+#include <QTcpSocket>
+#include <QDataStream>
+#include <QDateTime>
+#include <memory>
+#include "client.hpp"
+
+namespace spd {
+class ClientManager : public QObject {
+
+    Q_OBJECT
+
+public:
+    ClientManager();
+    ~ClientManager();
+
+    ClientManager(ClientManager const& a_other) = delete;
+    ClientManager& operator=(ClientManager const& a_other) = delete;
+
+    void connect_to_server(const QString &address, quint16 port);
+    void send_to_data_server();
+    void receive_event_from_server();
+
+    void register_device();
+
+private slots:
+    void onDataReceived();
+
+private:
+    QList<Client*> m_clients;
+    std::unique_ptr<QTcpSocket> m_socket;
+    quint16 m_block_size;
+};
+
+} // namespace name
+
+#endif //CLIENT_SERVER_MANAGER_HPP
---
+++ src/client_manager.cpp
@@ -0,0 +1,51 @@
+#include "client_manager.hpp"
+#include "event.hpp"
+
+namespace spd {
+
+spd::ClientManager::ClientManager()
+: m_socket{std::make_unique<QTcpSocket>(this)}
+, m_block_size{0}
+{
+    connect(m_socket.get(), &QTcpSocket::readyRead, this, &ClientManager::onDataReceived);
+}
+
+ClientManager::~ClientManager()
+{
+    m_socket->close();
+    for(auto &client: m_clients) {
+        delete client;
+    }
+}
+
+void ClientManager::onDataReceived() 
+{
+    QDataStream in(m_socket.get());
+
+    if (m_block_size == 0)
+    {
+        if (m_socket->bytesAvailable() < (int)sizeof(quint16))
+            return; 
+        in >> m_block_size;
+    }
+
+    if (m_socket->bytesAvailable() < m_block_size)
+    {
+       return;
+    }
+
+    QDateTime timeStamp;
+    QString eventType;
+    QString eventData;
+    QString eventLocation;
+    in >> timeStamp >> eventType >> eventData >> eventLocation;
+
+    m_block_size = 0; // Reset block size for next message
+}
+
+void ClientManager::connect_to_server(const QString &address, quint16 port)
+{
+    m_socket.get()->connectToHost(address, port);
+}
+} // namespace spd
+
