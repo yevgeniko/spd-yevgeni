870e88f5371c7c66ba58dea2bc3caf97bad80e21
h74.amirLitan <amirlitan@gmail.com>
2023-08-28 19:23:04 +0300

    add const data

--- inc/const_and_enums.hpp
+++ inc/const_and_enums.hpp
@@ -1,6 +1,12 @@
 #ifndef CONST_AND_ENUMS
 #define CONST_AND_ENUMS
 
+#include <cstdlib>
+#include <QString>
 
+const size_t WINDOW_WIDTH = 1120;
+const size_t WINDOW_HEIGHT = 636;
+const QString WINDOW_TITLE = "S.P.D.";
+const QString WIN_BACKGROUND_PATH = "../../assets/ward_blueprint.jpeg";
 
 #endif // CONST_AND_ENUMS
