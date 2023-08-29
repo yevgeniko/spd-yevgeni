6ac9a7baebc5a5dc2563e3362245af4df876a248
h74.amirLitan <amirlitan@gmail.com>
2023-08-29 21:37:53 +0300

    add intraction with new class

--- src/ui.cpp
+++ src/ui.cpp
@@ -6,12 +6,17 @@
 #include <QDebug>
 #include <QPainter>
 #include "const_and_enums.hpp"
+#include "ui_main_window.hpp"
+#include <QResizeEvent>
 
 namespace spd {
 
 UI::UI()
+: m_main_window{m_window}
 {
-   initwindow();
+    init_window();
+    init_room();
+    m_window.show();
 }
 
 void UI::show_event(QDateTime const& a_timeStamp, QString const& a_eventType, QString const& a_eventData, QString const& a_eventLocation)
@@ -21,22 +26,27 @@ void UI::show_event(QDateTime const& a_timeStamp, QString const& a_eventType, QS
                          + a_eventType + "\n" 
                          + a_eventData + "\n" 
                          + a_eventLocation + "\n";
-    m_rooms[0].get()->setText(message);
+    m_rooms[0]->setText(message);
 }
 
-void UI::initwindow()
+void UI::resizeEvent(QResizeEvent *event)
+{
+    QMainWindow::resizeEvent(event);
+    this->resize(event->size());
+    m_main_window.resizeEvent(*this);
+    m_main_window.resize(this->size());
+}
+
+void UI::init_window()
 {
     m_window.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     m_window.setWindowTitle(WINDOW_TITLE);
-    QPixmap backgroundImage(WIN_BACKGROUND_PATH); // Replace with your image path
-        
-    QPalette palette;
-    backgroundImage = backgroundImage.scaled(m_window.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);  
-    palette.setBrush(QPalette::Background, backgroundImage);
-    m_window.setAutoFillBackground(true);
-    m_window.setPalette(palette);
+}
 
-    m_window.show();
+void UI::init_room()
+{
+    m_rooms.append(new QPushButton("Room 1", &m_window));
+    m_rooms[0]->setGeometry(360, 45, 80, 80);
 }
 
 } // namespace spd
