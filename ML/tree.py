4e65dbddb1fd758833769a7bb80c5f8414c04d31
Yonatan Rosenberg <yonirosenberg98@gmail.com>
2023-09-06 15:58:09 +0300

    f

---
+++ ML/tree.py
@@ -0,0 +1,52 @@
+# import pandas as pd
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn import metrics
+import joblib
+import sys
+
+# # Function to train the Decision Tree Classifier and save it to a file
+# def train_and_save_classifier():
+#     col_names = ['pulse', 'body_temperature', 'spO2', 'status']
+#     # load dataset
+#     pima = pd.read_csv("Health data.csv", header=0, names=col_names)
+#
+#     feature_cols = ['pulse', 'body_temperature', 'spO2']
+#     X = pima[feature_cols]  # Features
+#     y = pima.status  # Target variable
+#
+#     # Split dataset into training set and test set
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)  # 70% training and 30% test
+#
+#     # Create Decision Tree classifer object
+#     clf = DecisionTreeClassifier()
+#
+#     # Train Decision Tree Classifer
+#     clf = clf.fit(X_train.values, y_train.values)
+#
+#     # Save the trained classifier to a file
+#     joblib.dump(clf, 'health_classifier.pkl')
+
+#Function to load the trained classifier and make predictions
+def load_classifier_and_predict(saturation, temp, pulse):
+    
+    clf = joblib.load('health_classifier.pkl')
+    prediction = int(clf.predict([[saturation, temp, pulse]]))
+    return prediction
+    
+
+
+if __name__ == "__main__":
+
+
+    saturation = float(sys.argv[1])
+    temp = float(sys.argv[2])
+    pulse = float(sys.argv[3])
+
+    new_patient_features = [[[saturation, temp, pulse]]]
+
+    predicted_health_status = load_classifier_and_predict(saturation, temp, pulse)
+    # print("Predicted Health Status:", predicted_health_status)
+    # print(2)
+
+
--- src/server_manager.cpp
+++ src/server_manager.cpp
@@ -58,8 +58,8 @@ void ServerManager::addAlert(const Event& event)
 
 void ServerManager::updateEventToRoomMap(int room_number, const Event& event) 
 {
-    qDebug() << "enqueued to event_to_room_map : " << event.getEventData();
     m_event_to_room_map[room_number].enqueue(event);
+    
 }
     //other sensor types as needed.
 
@@ -67,29 +67,21 @@ void ServerManager::updateEventToRoomMap(int room_number, const Event& event)
 
 void ServerManager::send_event(int room_number) 
 {
-    while(!m_event_to_room_map.value(room_number).empty()){
-        if (m_event_to_room_map.contains(room_number)) {
-            Event dequeued_Event = m_event_to_room_map[room_number].dequeue();
-            qDebug() << " sent event" << dequeued_Event.getEventType() << dequeued_Event.getEventType() << '\n'; 
-            m_simple_server_instance.forward_event_to_client(dequeued_Event);
-        }else{
-            qDebug() << " sent event function: not found. sending other one.  ";
-            
-            QString eventType = "ERROR";  
-            QString eventData = "0"; 
-            QString eventLocation = "NoWhere";  
-
-            QDateTime timestamp = QDateTime::currentDateTime();  
-
-            Event newEvent(timestamp, eventType, eventData, eventLocation);
-
-            m_simple_server_instance.forward_event_to_client(newEvent);
-        }
+    qDebug() << "\nRoom number accepterd by send_event function: " << room_number << '\n';
+    if(m_event_to_room_map.contains(room_number) && !m_event_to_room_map[room_number].isEmpty()){
+        Event dequeued_Event = m_event_to_room_map[room_number].dequeue();
+        qDebug() << " sent event" << dequeued_Event.getEventType() << dequeued_Event.getEventType() << '\n'; 
+        m_simple_server_instance.forward_event_to_client(dequeued_Event);
+        m_event_to_room_map[room_number].clear();
     }
+    // int alertsSent = 0;
     if (!m_alerts.isEmpty()) {
         Event alert_event = m_alerts.dequeue();
         m_simple_server_instance.forward_event_to_client(alert_event);
+        // alertsSent++;
     }
+    m_alerts.clear();
+
 }
 
 
