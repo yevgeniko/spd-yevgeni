8a7bffb06a31f38318ef2a9d3428316955f539db
h74.amirLitan <amirlitan@gmail.com>
2023-09-07 02:30:34 +0300

    add new way to handle event

--- inc/client.hpp
+++ inc/client.hpp
@@ -3,15 +3,26 @@
 
 #include <QString>
 #include <memory>
-#include "client_TCP.hpp"
-#include "ui.hpp"
 #include <QObject>
 #include <QThread>
 #include <QTimer>
+#include <QHash>
 
+#include "event.hpp"
+#include "client_TCP.hpp"
+#include "ui.hpp"
 
 
 namespace spd {
+
+struct CustomEventComparator {
+    bool operator()(Event const& a_first, Event const& a_second) const {
+        if(a_first.getEventType() == a_second.getEventType() && a_first.getEventLocation() == a_second.getEventLocation()){
+            return true;
+        }
+        return false;
+    }
+};
 
 class Client : public QObject {
     Q_OBJECT
@@ -27,12 +38,21 @@ private slots:
     void requestRoomEvents();
     void update_room_num(const int a_num);
     void checkAndRequestRoomEvents();
+private:
+    bool is_new_type(QString const& a_type);
+    size_t is_new_abnormal(Event const& a_abnormal);
+    bool is_timer_ended();
 
 private:
     ClientTCP m_manager;
     UI m_ui;
     int m_room_number;
+    QHash<QString,size_t> m_hash_type;
+    QList<Event> m_list_abnormal;
+    std::chrono::_V2::system_clock::time_point m_start_time;
 };
+
+
 
 } // namespace spd
 
--- src/client.cpp
+++ src/client.cpp
@@ -1,9 +1,13 @@
 #include "client.hpp"
+#include <chrono>
+#include <thread>
+#include "const_and_enums.hpp"
 
 namespace spd {
 
-Client::Client() :
-m_room_number {-1}
+Client::Client() 
+: m_room_number {-1}
+, m_hash_type{{"",0}}
 {
     connect(&m_manager, &ClientTCP::newDataReceived, this, &Client::handleNewData);
     connect(&m_ui, &UI::set_room_num, this, &Client::update_room_num);
@@ -17,14 +21,32 @@ void Client::connect_to_server(const QHostAddress &address, quint16 port)
 
 void Client::handleNewData(QDateTime const& a_timeStamp, QString const& a_eventType, QString const& a_eventData, QString const& a_eventLocation) 
 {
-    if (is_abnormal(a_eventType))
-    {
-        qDebug() << "IN Client::handleNewData WITH ABNORMAL EVENT";
-    } 
-        Event e(a_timeStamp, a_eventType, a_eventData, a_eventLocation); // object dies?
-        m_ui.add_event(e);
+    Event e(a_timeStamp, a_eventType, a_eventData, a_eventLocation);
+    if (is_abnormal(a_eventType)) {
 
-}
+        qDebug() << "IN Client::handleNewData WITH ABNORMAL EVENT";
+        
+        size_t postition = is_new_abnormal(e);
+
+        if(postition == 0) {
+            m_start_time = std::chrono::high_resolution_clock::now();
+            m_ui.add_abnorml_event(e);
+        } else {
+            m_ui.update_abnorml_event(e, postition - 1);
+        }
+    } else {
+        if(is_new_type(a_eventType)) {
+            m_ui.add_event(e);
+        } else {
+            m_ui.update_event(e, m_hash_type[a_eventType]);
+        }
+        m_ui.add_event(e);
+    }
+    if(is_timer_ended()) {
+        m_list_abnormal.clear();
+    }
+} 
+
 
 void Client::requestRoomEvents()
 {
@@ -40,6 +62,9 @@ void Client::update_room_num(const int a_num)
 {
     m_room_number = a_num;
     checkAndRequestRoomEvents();
+    if(m_room_number == 0 && !m_hash_type.empty()) {
+        m_hash_type.clear();
+    }
 }
 
 void Client::checkAndRequestRoomEvents()
@@ -48,6 +73,39 @@ void Client::checkAndRequestRoomEvents()
     {
         requestRoomEvents();
     }
+}
+
+bool Client::is_new_type(QString const& a_type)
+{
+    if(m_hash_type.count(a_type)) {
+        return false;
+    } else {
+        m_hash_type[a_type] = m_hash_type.size();
+        return true;
+    }
+}
+
+size_t spd::Client::is_new_abnormal(Event const &a_abnormal)
+{
+    size_t posistion = 0;
+    for(const auto &event: m_list_abnormal) {
+        ++posistion;
+        if(event.getEventLocation() == a_abnormal.getEventLocation() && event.getEventType() == a_abnormal.getEventType()) {
+            return posistion;
+        }
+    }
+    return posistion;
+}
+
+bool Client::is_timer_ended()
+{
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - m_start_time);
+
+    if (duration.count() >= MAX_ABNORMAL_DURATION) {
+        return true;
+    }
+    return false;
 }
 
 }
